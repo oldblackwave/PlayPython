22268b1f15e44743a46f30ba57b2a5aef2a4edb6
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-09-18 15:25:10 +0800

    json

--- Code/BookExample/ch02/ch02.py
+++ Code/BookExample/ch02/ch02.py
@@ -2,9 +2,24 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pylab
+import json
+
 from matplotlib.pyplot import plot
 from numpy import arange
 
 plt.plot(arange(20))
 
 pylab.show()
+
+
+path = 'usagov_bitly_data2012-03-16-1331923249.txt'
+print(open(path).readline())
+
+#_allLine = open(path).read()
+#print(_allLine)
+
+records = [json.loads(line) for line in open(path)]
+print(records[0])
+print(records[0]['tz'])
+
+
