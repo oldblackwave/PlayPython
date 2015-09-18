769cbc22f23159de21fd4fd3ac2677c43c5c830d
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-09-18 15:04:52 +0800

    ch02

---
+++ Code/BookExample/ch02/ch02.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pylab
+from matplotlib.pyplot import plot
+from numpy import arange
+
+plt.plot(arange(20))
+
+pylab.show()
