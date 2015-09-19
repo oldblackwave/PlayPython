d3aeac5f7adb2faa0766eb2814589d30b2cc715f
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-09-19 22:35:56 +0800

    ch02

--- Code/BookExample/ch02/ch02JSON.py
+++ Code/BookExample/ch02/ch02JSON.py
@@ -9,33 +9,21 @@ from numpy import arange
 from gc import get_count
 from collections import defaultdict
 
+from collections import Counter
+
 path = 'usagov_bitly_data2012-03-16-1331923249.txt'
 
-def get_counts(sequence):
-    counts = {}
-    for x in sequence:
-        if x in counts:
-            counts[x] += 1
-        else:
-            counts[x] = 1
-    return counts
 
-def get_counts2(sequence):
-    counts = defaultdict(int)
-    for x in sequence:
-        counts[x] += 1
-    return counts
 
 
 #records = [json.loads(line) for line in open(path)] #return List
-
+records = []
 for line in open(path):
-    records = []
     #print(type(line),line)
     records.append(json.loads(line))
 
 #print(type(records).records)
-
+'''
 print(type(records),records[0])
 print('\n')
 print(records[0]['a'])
@@ -48,17 +36,46 @@ print(records[0]['l'])
 print(records[0]['hh'])
 print(records[0]['r'])
 print('\n')
-
 '''
-time_zones = [rec['tz'] for rec in records if 'tz' in rec]
+###############################################################
+
+time_zones = [rec['tz'] for rec in records if 'tz' in rec]#Get all items with 'tz'
 print(time_zones)
-print(time_zones[:10])
+#print(time_zones[:10])
+print(len(time_zones))
 print('\n')
 
-_timezonesCounts = get_counts(time_zones)
-print(_timezonesCounts)
-print(_timezonesCounts['America/New_York'])
 
+###############################################################
+
+
+def get_counts(sequence):#Return Number
+    counts = {}
+    for x in sequence:
+        if x in counts:
+            counts[x] += 1
+        else:
+            counts[x] = 1
+    return counts
+
+def get_counts2(sequence):#Return Number
+    counts = defaultdict(int)
+    for x in sequence:
+        counts[x] += 1
+    return counts
+
+_timezonesCounts = get_counts(time_zones)
+#print(_timezonesCounts)
+print(_timezonesCounts['America/New_York'])
+print(len(_timezonesCounts))
+print('\n')
+
+_timezonesCounts2 = get_counts2(time_zones)
+print(_timezonesCounts2['America/New_York'])
+print(len(_timezonesCounts2))
+print('\n')
+###############################################################
+'''
 _hRec = [rec['h'] for rec in records if 'h' in rec]
 print(type(_hRec),_hRec)
 _hRecCount = get_counts(_hRec)
@@ -74,3 +91,28 @@ print(_hRecCount2['xaOGZY'])
 
 print('\n')
 '''
+###############################################################
+
+def top_counts(count_dict, n=10):
+    value_key_pairs = [(count,tz) for tz, count in count_dict.items()]
+    value_key_pairs.sort()
+    print(type(value_key_pairs),type(value_key_pairs[0]))
+    return value_key_pairs
+
+def top_counts2(count_dict, n = 10):
+    value_key_pairs = []
+    for tz, count in count_dict.items():
+        value_key_pairs.append([count,tz])
+    value_key_pairs.sort()
+    return value_key_pairs
+
+print(top_counts(_timezonesCounts))
+print(top_counts2(_timezonesCounts))
+
+###############################################################
+
+_timeZoneCollectionCounts = Counter(time_zones)
+print(_timeZoneCollectionCounts.most_common(10))
+
+###############################################################
+
