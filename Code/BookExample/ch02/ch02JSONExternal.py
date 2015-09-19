22e41d0af539911317d4f308e531400b7c55f566
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-09-19 14:46:55 +0800

    ch02

--- Code/BookExample/ch02/ch02JSONExternal.py
+++ Code/BookExample/ch02/ch02JSONExternal.py
@@ -1,7 +1,18 @@
 import json
 
+orig = '''{"name":"test","type":"TT"}
+        {"name":"test","type":"TT"}
+        {"name":"test","type":"TT"}
+        {"name":"test","type":"TT"}'''
+        
 s = json.loads('{"name":"test","type":"TT"}')
 print(s)
 print(s.keys())
 print(s["name"])
 print(s["type"])
+
+print('\n')
+
+for line in orig.split('\n'):
+    orig_doc = json.loads(line)
+    print(orig_doc)
