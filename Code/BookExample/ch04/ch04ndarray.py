73e1ce7dc01315cf988bb54b57ff1a689bd19453
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-09-29 22:38:47 +0800

    ch04

---
+++ Code/BookExample/ch04/ch04ndarray.py
@@ -0,0 +1,10 @@
+import numpy as np
+from numpy.core.multiarray import dtype
+
+arr1 = np.array([1,2,3], dtype=np.float64)
+arr2 = np.array([1,2,3], dtype=np.int32)
+print(arr1.dtype)
+print(arr2.dtype)
+
+###############################################################
+
