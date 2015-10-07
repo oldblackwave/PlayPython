e259496d3c65893529abb1267f3b888998d5a29b
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-10-08 00:27:30 +0800

    ch04

--- Code/BookExample/ch04/ch04Random.py
+++ Code/BookExample/ch04/ch04Random.py
@@ -39,10 +39,40 @@ walk = steps.cumsum()
 
 print(walk.min())
 print(walk.max())
+print((np.abs(walk)>=10).argmax())
+print('\n')
+###############################################################
 
+nwalks = 5000
+nsteps = 1000
+draws = np.random.randint(0,2,size=(nwalks,nsteps))
+#print(draws)
+steps = np.where(draws>0, 1, -1)
+#print(steps)
+walks = steps.cumsum(1)
+print(walks)
+print('\n')
+print(walks.max())
+print(walks.min())
 
+hits30 = (np.abs(walks) >= 30).any(1)
+print(hits30)
+
+print(hits30.sum())
 
 ###############################################################
+
+crossing_times = (np.abs(walks[hits30]) >= 30).argmax(1)
+print(crossing_times.mean())
+
+print('\n')
+
+steps = np.random.normal(loc=0, scale=0.25, size=(nwalks,nsteps))
+
+
+
+
+
 
 
 
