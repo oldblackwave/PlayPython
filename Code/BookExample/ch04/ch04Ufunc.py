9dfba86780a9e691fd5bff79c79698f136afd7d9
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-10-07 12:37:46 +0800

    ch04

--- Code/BookExample/ch04/ch04Ufunc.py
+++ Code/BookExample/ch04/ch04Ufunc.py
@@ -54,6 +54,107 @@ result = np.where(cond, xarr, yarr)
 print(result)
 print('\n')
 
+###############################################################
+
+arr = randn(4,4)
+print(arr)
+print('\n')
+
+print(np.where(arr > 0,2,-2))
+print('\n')
+
+print(np.where(arr>0,2,arr))
+print('\n')
+
+###############################################################
+
+arr = np.random.randn(5,4)
+print(arr.mean())
+print(np.mean(arr))
+print(arr.sum())
+print('\n')
+
+print(arr.mean(axis = 1))
+print(arr.sum(0))
+
+###############################################################
+
+arr = np.array([[0,1,2],[3,4,5],[6,7,8]])
+print(arr)
+print(arr.cumsum(0))
+print(arr.cumprod(1))
+print('\n')
+
+###############################################################
+
+arr = randn(100)
+print((arr>0).sum())
+print('\n')
+
+###############################################################
+
+bools = np.array([False, False, True, False])
+print(bools.any())
+print(bools.all())
+print('\n')
+
+###############################################################
+
+arr = randn(8)
+print(arr)
+arr.sort()
+print(arr)
+
+print('\n')
+
+arr = randn(5,3)
+print(arr)
+arr.sort(1)
+print(arr)
+
+print('\n')
+
+large_arr = randn(100)
+large_arr.sort()
+print(large_arr[int(0.05 * len(large_arr))])
+
+print('\n')
+
+
+###############################################################
+
+names = np.array(['Bob','Joe','Will','Bob','Will','Joe','Joe'])
+print(np.unique(names))
+
+ints = np.array([3,3,3,2,2,1,1,4,4])
+print(np.unique(ints))
+
+
+print(sorted(set(names)))
+print('\n')
+
+values = np.array([6,0,0,3,2,5,6])
+print(np.in1d(values, [2,3,6]))
+print('\n')
+
+
+###############################################################
+
+
+
+
+
+
+
+
+
+###############################################################
+
+
+
+
+
+
 
 
 
