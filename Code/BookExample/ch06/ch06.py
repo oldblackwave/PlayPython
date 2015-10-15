0e52f83bbd787b397176d61cce17d3acc6939bfd
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-10-15 21:58:04 +0800

    ch06

---
+++ Code/BookExample/ch06/ch06.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import pandas.io.data as web
+
+import pandas.io.data as web
+
+from pandas import Series, DataFrame
+from numpy import NaN as NA
+
+###############################################################
+
+ex1 = pd.read_csv('data/ex1.csv', sep=',')
+print(ex1)
+
+
+
