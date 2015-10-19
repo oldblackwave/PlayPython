1665afd97520b66818c4ea614a2d1f4e7fad1e43
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-10-19 12:46:09 +0800

    ch06

---
+++ Code/BookExample/ch06/ch06Handling.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import pandas.io.data as web
+import sys 
+
+import pandas.io.data as web
+
+from pandas import Series, DataFrame
+from numpy import NaN as NA
+
+###############################################################
+
+
+
