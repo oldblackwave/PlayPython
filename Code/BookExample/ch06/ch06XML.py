3b20f2d541428cdebac47f3a5b0c68997e1822f4
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-10-27 17:22:41 +0800

    ch06

--- Code/BookExample/ch06/ch06XML.py
+++ Code/BookExample/ch06/ch06XML.py
@@ -18,5 +18,23 @@ from lxml import objectify
 ###############################################################
 
 
+path = 'data/Performance_MNR.xml'
+parsed = objectify.parse(open(path))
+root = parsed.getroot()
+
+data = []
+skip_fields = ['PARENT_SEQ', 'INDICATOR_SEQ', 'DESIRED_CHANGE', 'DECIMAL_PLACES']
+
+for elt in root:
+    el_data = {}
+    for child in elt.getchildren():
+        if child.tag in skip_fields:
+            continue
+        el_data[child.tag]=child.pyval
+    data.append(el_data)
+    
+perf = DataFrame(data)
+print(perf)
+
 
 
