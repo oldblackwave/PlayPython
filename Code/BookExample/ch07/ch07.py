78c721342ac282c0cfd03194a7c166c9e5c410ea
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-11-16 14:19:32 +0800

    ch07

--- Code/BookExample/ch07/ch07.py
+++ Code/BookExample/ch07/ch07.py
@@ -1,6 +1,42 @@
 import numpy as np
 import pandas as pd
 import pandas.io.data as web
-
+import sys 
+import csv
 import json
+import urllib
+
+import pandas.io.data as web
+
+from pandas import Series, DataFrame
+from pandas.io.parsers import TextParser
+from numpy import NaN as NA
+from lxml.html import parse
+from urllib.request import urlopen
+from lxml import objectify
+from io import StringIO
+
+###############################################################
+
+df1 = DataFrame({'key':['b','b','a','c','a','a','b'],
+                 'data1':range(7)})
+df2 = DataFrame({'key':['a','b','d'],
+                 'data2':range(3)})
+
+print(df1)
+print('\n')
+print(df2)
+print('\n')
+
+print(pd.merge(df1,df2))
+
+print('\n')
+
+print(pd.merge(df1,df2,on='key'))
+
+
+
+
+
+
 
