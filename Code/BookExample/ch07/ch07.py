239582dd713f895b8f6ee682be53a1ffe4dca886
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-11-22 17:24:46 +0800

    ch07

--- Code/BookExample/ch07/ch07.py
+++ Code/BookExample/ch07/ch07.py
@@ -52,6 +52,43 @@ print(pd.merge(df3, df4, left_on='lkey', right_on='rkey'))
 
 print('\n')
 
+print(pd.merge(df1, df2, how='outer'))
+
+print('\n')
+
+df1 = DataFrame({'key':['b','b','a','c','a','b'],
+                 'data1':range(6)})
+
+df2 = DataFrame({'key':['a','b','a','b','d'],
+                 'data2':range(5)})
+
+print(df1)
+print('\n')
+print(df2)
+print('\n')
+
+print(pd.merge(df1,df2, on='key', how='left'))
+print('\n')
+
+print(pd.merge(df1,df2,how='inner'))
+print('\n')
+
+left = DataFrame({'key1':['foo','foo','bar'],
+                  'key2':['one','two','one'],
+                  'lval':[1,2,3]})
+
+right = DataFrame({'key1':['foo','foo','bar','bar'],
+                   'key2':['one','one','one','two'],
+                   'rval':[4,5,6,7]})
+
+print(left)
+print(right)
+print('\n')
+print(pd.merge(left,right, on=['key1','key2'], how='outer'))
+print('\n')
+
+
+
 
 
 
