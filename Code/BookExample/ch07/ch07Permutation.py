50c5e0d9f565da067489be9c9c1580ccb206509e
OLDBLACKWAVE <oldblackwave@hotmail.com>
2015-11-27 15:18:49 +0800

    ch07

--- Code/BookExample/ch07/ch07Permutation.py
+++ Code/BookExample/ch07/ch07Permutation.py
@@ -23,3 +23,41 @@ sampler = np.random.permutation(5)
 print(sampler)
 print('\n')
 
+print(df)
+print(df.take(sampler))
+print('\n')
+print(df.take(np.random.permutation(len(df))[:3]))
+
+bag = np.array([5,7,-1,6,4])
+
+sampler = np.random.randint(0,len(bag), size=10)
+
+print(sampler)
+print('\n')
+
+draws = bag.take(sampler)
+
+print(draws)
+print('\n')
+
+###############################################################
+
+df = DataFrame({'key':['b','b','a','c','a','b'],
+                'data1':range(6)})
+print(df)
+print(pd.get_dummies(df['key']))
+print('\n')
+
+dummies = pd.get_dummies(df['key'],prefix='key')
+
+df_with_dummy = df[['data1']].join(dummies)
+print(df_with_dummy)
+print('\n')
+
+
+###############################################################
+
+mnames = ['moive_id', 'title', 'genres']
+moives = pd.read_table('data/moives.dat', sep='::', header=None, names=mnames)
+
+
